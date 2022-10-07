49360df2b1f691f035b1f80cb4af8d39132a69e3
Dioegox <degalvez@miuandes.cl>
2022-10-07 15:42:44 -0300

    first commit

---
+++ testjs.js
@@ -0,0 +1,15 @@
+
+const input = prompt("Ingrese un numero:");
+fn1=0;
+fn2=1;
+index=1;
+if(input>1){
+    while(index<prompt){
+        fn2=fn2+fn1;
+    }
+    console.log("f(",input,") = ",fn2);
+}
+else{
+    console.log("numero invalido");
+}
+
